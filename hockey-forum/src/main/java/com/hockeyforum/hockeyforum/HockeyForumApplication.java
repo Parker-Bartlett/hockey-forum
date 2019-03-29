f5babcaac47ee574aeed695c8dd6ccb2c1f1c1c7
Parker-Bartlett <prbartlett1@gmail.com>
2019-03-29 15:45:19 -0400

    initial commit

---
+++ hockey-forum/src/main/java/com/hockeyforum/hockeyforum/HockeyForumApplication.java
@@ -0,0 +1,13 @@
+package com.hockeyforum.hockeyforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HockeyForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HockeyForumApplication.class, args);
+	}
+
+}
---
+++ hockey-forum/src/test/java/com/hockeyforum/hockeyforum/HockeyForumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hockeyforum.hockeyforum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HockeyForumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
